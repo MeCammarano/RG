2cc97baa73730c14d56a7ab7c0a868389f746f79
mariaelena <m.cammarano25@studenti.unisa.it>
2018-11-14 12:59:24 +0100

    Added Partial Update

--- angular/src/app/table/table.component.ts
+++ angular/src/app/table/table.component.ts
@@ -19,4 +19,13 @@ export class TableComponent implements OnInit {
     this.data = this.data.map((x, i) => this.data[dataLen - i]);
   }
 
+  partialUpdate() {
+    const dataLen = this.data.length - 1;
+    this.data = this.data.map((x, i) => {
+      if(i >= ((dataLen)/10))
+        return x;
+      return this.data[(i+1)*9+i];
+    })
+  }
+
 }
