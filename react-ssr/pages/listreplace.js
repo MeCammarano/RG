63b0b404d4c8641fa6dd77aafb25bdd29a85fccf
Luca <lcpp1994@gmail.com>
2018-12-05 16:44:10 +0100

    updated listreplace

--- react-ssr/pages/listreplace.js
+++ react-ssr/pages/listreplace.js
@@ -1,13 +1,20 @@
 import React from 'react'
+import 'isomorphic-fetch';
 
 export default class extends React.Component {
-    static async getInitialProps({ req }) {
-        const res = await fetch('http://localhost:3000/static/data.json')
-        const data = await res.json();
-        return { data }
+    static async getInitialProps({ req, query }) {
+        const promises = [];
+        try {
+            for (let i = 0; i < query.rep; i++)
+                promises.push(await fetch('http://localhost:3000/static/data.json').then(data => data.json()))
+            return { data: await Promise.all(promises) }
+        }
+        catch (err) {
+            console.error(err);
+        }
     }
 
     render() {
-        return this.props.data.map(x => <p>{x.name}</p>)
+        return this.props.data.map(x => x.map((y, i) => <p key={'user-' + i}>{y.name}</p>))
     }
 }
