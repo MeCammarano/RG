4a6474cc52fedbdd555cfab1f8b275fa3a570fae
Luca <lcpp1994@gmail.com>
2018-12-07 22:23:52 +0100

    implemented data scrambling and improved styling

--- react-ssr/pages/listreplace.js
+++ react-ssr/pages/listreplace.js
@@ -1,5 +1,19 @@
-import React from 'react'
+import React from 'react';
 import 'isomorphic-fetch';
+
+const divStyle = {
+    textAlign: 'center'
+}
+
+const scrambleData = (data) => {
+    for (let value of data) {
+        for (let i = 0; i < 100000000; i++) {
+            const [num1, num2] = [Math.floor(Math.random() * value.length), Math.floor(Math.random() * value.length)];
+            [value[num1], value[num2]] = [value[num2], value[num1]];
+        }
+    }
+    return data;
+}
 
 export default class extends React.Component {
     static async getInitialProps({ req, query }) {
@@ -7,7 +21,7 @@ export default class extends React.Component {
         try {
             for (let i = 0; i < query.rep; i++)
                 promises.push(await fetch('http://localhost:3000/static/data.json').then(data => data.json()))
-            return { data: await Promise.all(promises) }
+            return { data: scrambleData(await Promise.all(promises)) }
         }
         catch (err) {
             console.error(err);
@@ -15,6 +29,14 @@ export default class extends React.Component {
     }
 
     render() {
-        return this.props.data.map(x => x.map((y, i) => <p key={'user-' + i}>{y.name}</p>))
+        return (
+            <div style={divStyle}>
+                <h2>List and Replace</h2>
+                <h2>Size: {this.props.data ? this.props.data.length * this.props.data[0].length : null}</h2>
+                <ol>
+                    {this.props.data.map(x => x.map((user, i) => <li key={'user-' + i}>{user.name} ({user.email})</li>))}
+                </ol>
+            </div>
+        )
     }
 }
